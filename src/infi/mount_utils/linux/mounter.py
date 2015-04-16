0ffa6274a0f0fe84b1cc77b5f02e271f6abab411
Guy Rozendorn <guy@rzn.co.il>
2015-04-16 15:48:38 +0300

    TRIVIAL fixing LinuxMounterMixin

--- src/infi/mount_utils/linux/mounter.py
+++ src/infi/mount_utils/linux/mounter.py
@@ -2,7 +2,7 @@ from ..base.mounter import MounterMixin, execute_mount
 
 class LinuxMounterMixin(MounterMixin):
     def _get_entry_format(self, entry):
-        return entry.get_format_linux()
+        return str(entry)
 
     def mount_entry(self, entry):
         args = ["-t", entry.get_typename(), entry.get_fsname(), entry.get_dirname()]
